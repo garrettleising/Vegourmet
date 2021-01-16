c9aab6de1841bbb6b9ce08db9d66b8aae4884b48
Garrett <gzleising@gmail.com>
2021-01-15 22:52:59 -0800

    expo pizza app

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { Text, View, TextInput } from 'react-native';
+
+export default function App() {
+  const [text, setText] = useState('');
+  return (
+    <View style={{padding: 10}}>
+      <TextInput
+        style={{height: 40}}
+        placeholder="Type here to translate!"
+        onChangeText={text => setText(text)}
+        defaultValue={text}
+      />
+      <Text style={{padding: 10, fontSize: 42}}>
+        {text.split(' ').map((word) => word && '🍕').join(' ')}
+      </Text>
+    </View>
+  );
+}
