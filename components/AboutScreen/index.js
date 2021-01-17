1a95e6832c808f8d092b4dd6e2a7acadc16a76cb
Garrett <gzleising@gmail.com>
2021-01-17 03:01:48 -0800

    added import for about page

--- components/AboutScreen/index.js
+++ components/AboutScreen/index.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 import {
   yellowGreen,
   chestnut,
