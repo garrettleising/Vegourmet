075f9538e9e92a12cc0236e84b0b6792ddf926c3
Garrett <gzleising@gmail.com>
2021-01-16 20:03:26 -0800

    removed uneeded import

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { View, StyleSheet, Text, Button, ScrollView } from "react-native";
 // import { getFoodRepoToken, foodRepoParseName, foodRepoRequest } from "./Leo.js";
 
