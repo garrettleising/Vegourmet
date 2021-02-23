62d1eacf8693a4f16825f215c128339f95e08b02
Garrett Leising <48079037+garrettleising@users.noreply.github.com>
2021-02-22 16:52:45 -0800

    Update APIKeys.js
    updated keys

--- APIKeys.js
+++ APIKeys.js
@@ -1,2 +1,2 @@
-export var getBonApiToken = "322fc7c0f84cc96314025db96c8771c6f82fa68f";
-export var getFoodRepoToken = "a59b2571500290e31d6b0c0629aca8fd";
+export var getBonApiToken = "";
+export var getFoodRepoToken = "";
